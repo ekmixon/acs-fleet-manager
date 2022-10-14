38d590821ce7a499b501a241333b5caf6ebdd113
Sourcery AI <>
2022-10-14 14:49:59 +0000

    'Refactored by Sourcery'

--- e2e/loadtest/locustfile.py
+++ e2e/loadtest/locustfile.py
@@ -7,7 +7,9 @@ class ListCentrals(HttpUser):
     @task
     def get_centrals(self):
         token = os.getenv('STATIC_TOKEN')
-        self.client.get("/api/rhacs/v1/centrals", headers={"authorization": "Bearer " + token})
+        self.client.get(
+            "/api/rhacs/v1/centrals", headers={"authorization": f"Bearer {token}"}
+        )
 
     # Another task
     # See details: https://docs.locust.io/en/stable/writing-a-locustfile.html#tasks
